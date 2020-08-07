dc66a3df77bbe4d26d85655733a61cbb2c4b9131
Ahmed AbulMagd <abulmagd@gmail.com>
2020-08-07 20:12:40 +0200

    made few components

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RegFormComponent} from './regform/regform.component';
+import {LoginFormComponent} from './loginform/loginform.component';
+import {ItemViewComponent} from './itemv/itemv.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegFormComponent,
+    LoginFormComponent,
+    ItemViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ app/itemv/itemv.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itemview',
+  templateUrl: './itemv.component.html',
+  styleUrls: ['./itemv.component.css']
+})
+export class ItemViewComponent {
+ // title = 'Login';
+}
